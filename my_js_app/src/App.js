a60986ab1eca7d164e34f3914eda210ca65e2fe4
Michael Yin <me@micy.in>
2022-06-01 11:56:27 +1200

    implement the javascript app

--- my_js_app/src/App.js
+++ my_js_app/src/App.js
@@ -1,24 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ApolloClient, InMemoryCache, useQuery, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
+  const { data, loading, error } = useQuery(
+    gql`
+      query App {
+        posts {
+          id
+        }
+
+        authors {
+          id
+          name
+
+          posts {
+            id
+            title
+          }
+        }
+      }
+    `,
+    { client }
+  );
+
+  if (loading) return "loading...";
+  if (error) return `error: ${error.message}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Posts</p>
+      <ul>
+        {data?.posts.map((post) => (
+          <Post key={post.id} postId={post.id} />
+        ))}
+      </ul>
+      <p>Authors</p>
+      <ul>
+        {data?.authors.map((author) => (
+          <li key={author.id}>
+            <i>{author.name}</i>:
+            {author.posts.map((post) => (
+              <b key={post.id}>{post.title}; </b>
+            ))}
+          </li>
+        ))}
+      </ul>
     </div>
+  );
+}
+
+function Post({ postId }) {
+  const [expanded, expandedSet] = useState(false);
+
+  const { data, loading, error } = useQuery(
+    gql`
+      query Post($postId: ID!) {
+        post(id: $postId) {
+          id
+          title
+          content
+          createdAt
+
+          comments {
+            id
+            content
+            createdAt
+          }
+
+          author {
+            id
+            name
+          }
+        }
+      }
+    `,
+    { client, variables: { postId } }
+  );
+
+  if (loading) return "loading...";
+  if (error) return `error: ${error.message}`;
+
+  return (
+    <li>
+      {data?.post.title} by <i>{data?.post.author.name}</i>
+      <a
+        href="#"
+        onClick={(event) => {
+          event.preventDefault();
+          expandedSet((e) => !e);
+        }}
+      >
+        [{expanded ? "-" : "+"}]
+      </a>
+      {expanded && (
+        <>
+          <p>Created At: {data?.post.createdAt}</p>
+          <p
+            style={{
+              whiteSpace: "pre-wrap",
+            }}
+          >
+            {data?.post.content}
+          </p>
+          <p>comments:</p>
+          {data?.post.comments.map((comment) => (
+            <p key={comment.id}>
+              {comment.createdAt}: {comment.content}
+            </p>
+          ))}
+        </>
+      )}
+    </li>
   );
 }
 
