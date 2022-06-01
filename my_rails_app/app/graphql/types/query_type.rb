ba5098df04346090b8d28b5b29cd5dc7d7a93d1b
Michael Yin <me@micy.in>
2022-06-01 12:19:02 +1200

    add root-level fields

--- my_rails_app/app/graphql/types/query_type.rb
+++ my_rails_app/app/graphql/types/query_type.rb
@@ -7,11 +7,21 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :posts, [PostType], null: false
+    def posts
+      Post.all
+    end
+
+    field :authors, [AuthorType], null: false
+    def authors
+      Author.all
+    end
+
+    field :post, PostType, null: false do
+      argument :id, ID, required: true
+    end
+    def post(id:)
+      Post.find(id)
     end
   end
 end
